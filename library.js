46a541320d31750897c5ea5ebec58af8ee7fda2f
retra <jozalamun@gmail.com>
2024-07-03 09:52:47 -0400

    Change form validation from minlength to require

--- library.js
+++ library.js
@@ -11,13 +11,13 @@ class Book {
   }
 
   toggleStatus() {
-    this.status = this.status === 'read' ? 'not read' : 'read';
+    this.status = this.status === "read" ? "not read" : "read";
   }
 }
 
 class Library {
   constructor() {
-    this.books = [theHobbit]
+    this.books = [theHobbit];
   }
 
   addBook() {
@@ -26,40 +26,38 @@ class Library {
     const pages = document.getElementById("pageCount").value;
 
     const statusCheckbox = document.getElementById("status");
-    const status = statusCheckbox.checked ? 'read' : 'not read';
+    const status = statusCheckbox.checked ? "read" : "not read";
 
-    const newBook = new Book(title, author, pages, status)
+    const newBook = new Book(title, author, pages, status);
 
-    this.books.push(newBook)
+    this.books.push(newBook);
 
-      
     // should be a private method?
     this.listBooks();
   }
 
-// should be a private method?
+  // should be a private method?
   listBooks() {
     const parent = document.querySelector(".book-container");
-    
+
     // clear book container
-    parent.innerHTML = '';
+    parent.innerHTML = "";
 
     // set a book Id index to associate with each book
     for (let bookId = 0; bookId < this.books.length; bookId++) {
-
       const newCard = document.createElement("div");
       newCard.setAttribute("class", "book");
       newCard.setAttribute("id", `book${bookId}`);
-      
+
       const currentBook = this.books[bookId];
 
-      newCard.innerHTML = currentBook.details()
+      newCard.innerHTML = currentBook.details();
 
       // handles deleting added books
       const deleteBtn = document.createElement("button");
       deleteBtn.setAttribute("type", "button");
       deleteBtn.innerHTML = "Delete";
-      deleteBtn.addEventListener("click", function() {
+      deleteBtn.addEventListener("click", function () {
         // const bookId = myLibrary.books.indexOf(currentBook);
         myLibrary.books.splice(bookId, 1);
 
@@ -73,51 +71,52 @@ class Library {
       // handles toggling read status
       const toggleStatusBtn = document.createElement("button");
       toggleStatusBtn.setAttribute("type", "button");
-      toggleStatusBtn.innerHTML = 'Toggle Status';
-      toggleStatusBtn.addEventListener("click", function() {
+      toggleStatusBtn.innerHTML = "Toggle Status";
+      toggleStatusBtn.addEventListener("click", function () {
         currentBook.toggleStatus();
-        newCard.innerHTML = 
-        console.log(currentBook.status);
+        newCard.innerHTML = console.log(currentBook.status);
         myLibrary.listBooks();
-      })
+      });
 
       parent.appendChild(newCard);
       newCard.appendChild(deleteBtn);
       newCard.appendChild(toggleStatusBtn);
-    };
-  };
+    }
+  }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', 310, 'read');
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", 310, "read");
 const myLibrary = new Library();
 const showButton = document.getElementById("showDialog");
 const newBookDialog = document.getElementById("newBookDialog");
 const outputBox = document.querySelector(".book-container");
-const confirmBtn = newBookDialog.querySelector("#confirmBtn")
+const confirmBtn = newBookDialog.querySelector("#confirmBtn");
 
 showButton.addEventListener("click", () => {
   newBookDialog.showModal();
 });
 
 newBookDialog.addEventListener("close", (e) => {
-  outputBox.value = 
+  outputBox.value =
     newBookDialog.returnValue === "default"
       ? "No return value."
       : `ReturnValue: ${newBookDialog.returnValue}.`;
 
-      // clear modal fields
-      document.getElementById("title").value = "";
-      document.getElementById("author").value = "";
-      document.getElementById("pageCount").value = "";
-      document.getElementById("status").checked = false;
+  // clear modal fields
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pageCount").value = "";
+  document.getElementById("status").checked = false;
 });
 
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault();
+document
+  .getElementById("bookForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  myLibrary.addBook();
+    myLibrary.addBook();
 
-  newBookDialog.close();
-});
+    newBookDialog.close();
+  });
 
 myLibrary.listBooks();
